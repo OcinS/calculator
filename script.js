2527bf8d73ebf2e9ad5d78d61d76b5040d6cc51a
Nico Sison <nicodsison@gmail.com>
2023-10-11 15:20:58 +0800

    Update JS and HTML file
    Test on the functions

---
+++ script.js
@@ -0,0 +1,28 @@
+let firstOperand = 0;
+let secondOperand = 5;
+let operator = 0;
+
+let display = 0;
+
+
+const numberButtons = document.querySelectorAll(`.number.btn`);
+
+numberButtons.forEach(() => {
+    
+});
+
+function add(a,b) {  
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
